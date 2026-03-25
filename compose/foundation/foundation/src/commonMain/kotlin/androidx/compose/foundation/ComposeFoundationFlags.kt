3cdf2aa3a085057724d613bbc1fce7bcd844b885
Levi Albuquerque <levima@google.com>
2026-03-25 09:56:54 +0000

    Remove flag isBringIntoViewRltBouncyBehaviorInPagerFixEnabled
    Remove flag isBringIntoViewRltBouncyBehaviorInPagerFixEnabled as it's been released in a stable version.
    
    Fixes: 485967682
    Test: Previous tests should pass.
    Relnote: Remove flag isBringIntoViewRltBouncyBehaviorInPagerFixEnabled as it's been released in a stable version.
    Change-Id: I73fb27dc6e95d97c1b374e43e5678a4cb19c8047

--- compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/ComposeFoundationFlags.kt
+++ compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/ComposeFoundationFlags.kt
@@ -97,15 +97,6 @@ object ComposeFoundationFlags {
     @field:Suppress("MutableBareField") @JvmField var isCacheWindowForPagerEnabled = true
 
     /**
-     * When Pager was used with a keyboard in RTL the pages would bounce indefinitely due to the
-     * bring into view animation. If this flag is off the fix for that behavior will be disabled.
-     */
-    // TODO: b/485967682
-    @field:Suppress("MutableBareField")
-    @JvmField
-    var isBringIntoViewRltBouncyBehaviorInPagerFixEnabled: Boolean = true
-
-    /**
      * With this flag enabled,
      * [androidx.compose.foundation.gestures.AnchoredDraggableState.targetValue] correctly returns
      * the [androidx.compose.foundation.gestures.AnchoredDraggableState.currentValue] when no
