238fd76ed4e83c426307261d66a83826ec7f3081
Jordan Saleh <jsaleh@google.com>
2026-03-30 10:58:15 -0700

    Replace AnchorEntity getAnchor with anchor
    This change also sets the anchor property when the AnchorEntity was created from an ARCore anchor
    
    There were no g3 references found for this API
    
    Bug: 487012373
    Bug: 487693335
    Test: Unit Test, Manual test
    RelNote: `AnchorEntity.getAnchor` was replaced with `AnchorEntity.anchor` and will now return an `Anchor` when the `AnchorEntity` was created from an ARCore `Anchor`
    Change-Id: I5c7c8c9457e4590d4652dd0199c079fa24b3066f

--- xr/scenecore/scenecore/src/test/java/androidx/xr/scenecore/AnchorEntityTest.kt
+++ xr/scenecore/scenecore/src/test/java/androidx/xr/scenecore/AnchorEntityTest.kt
@@ -108,6 +108,13 @@ class AnchorEntityTest {
     }
 
     @Test
+    fun createViaAnchor_anchor_returnsProvidedAnchor() {
+        val anchorEntity = AnchorEntity.create(session, anchor)
+
+        assertThat(anchorEntity.anchor).isEqualTo(anchor)
+    }
+
+    @Test
     fun createViaSemantic_noPlanes_returnsUnanchoredEntity() {
         val anchorEntity =
             AnchorEntity.create(
@@ -187,7 +194,7 @@ class AnchorEntityTest {
             advanceUntilIdle()
 
             assertThat(anchorEntity.state).isEqualTo(AnchorEntity.State.ANCHORED)
-            val anchor1 = anchorEntity.getAnchor()
+            val anchor1 = anchorEntity.anchor
             assertThat(anchor1).isNotNull()
 
             // Add another matching plane
@@ -202,7 +209,7 @@ class AnchorEntityTest {
 
             // Should still be anchored to the first one
             assertThat(anchorEntity.state).isEqualTo(AnchorEntity.State.ANCHORED)
-            assertThat(anchorEntity.getAnchor()).isEqualTo(anchor1)
+            assertThat(anchorEntity.anchor).isEqualTo(anchor1)
         }
     }
 
