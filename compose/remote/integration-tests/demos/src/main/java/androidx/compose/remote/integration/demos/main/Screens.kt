b5cf94ca5b545c08dbbe8d2d9b93bcc827f7cbba
Gustavo Pagani <gustavopagani@google.com>
2026-04-08 21:25:11 +0000

    Change padding modifier to accept start/end parameters instead of left/right.
    Bug: 500760020
    Relnote: "padding modifier parameters were changed from left/right to start/end."
    Test: screenshot tests added.
    Change-Id: Id781cdd4c631488e0934555dcbbb0d7c54989741

--- compose/remote/integration-tests/demos/src/main/java/androidx/compose/remote/integration/demos/main/Screens.kt
+++ compose/remote/integration-tests/demos/src/main/java/androidx/compose/remote/integration/demos/main/Screens.kt
@@ -21,6 +21,7 @@ import androidx.compose.remote.integration.demos.layout.RemoteBoxAlignmentsDemo
 import androidx.compose.remote.integration.demos.layout.RemoteStateLayoutSimpleDemo
 import androidx.compose.remote.integration.demos.modifier.ClickableDemo
 import androidx.compose.remote.integration.demos.modifier.CombinedClickableDemo
+import androidx.compose.remote.integration.demos.modifier.PaddingDemo
 import androidx.compose.remote.integration.demos.modifier.TouchActionDemo
 import androidx.compose.remote.integration.demos.player.BitmapLoaderDemo
 import androidx.compose.remote.integration.demos.settings.SettingsScreen
@@ -31,6 +32,7 @@ private object ScreenKeys {
     const val REMOTE_STATE_LAYOUT = "REMOTE_STATE_LAYOUT"
     const val CLICKABLE = "CLICKABLE"
     const val COMBINED_CLICKABLE = "COMBINED_CLICKABLE"
+    const val PADDING = "PADDING"
     const val TOUCH_ACTION = "TOUCH_ACTION"
     const val BITMAP_LOADER = "BITMAP_LOADER"
     const val SETTINGS = "SETTINGS"
@@ -41,6 +43,7 @@ fun ComposableScreenNavigation(key: String, onNavigateUp: () -> Unit) {
     when (key) {
         ScreenKeys.REMOTE_BOX_ALIGNMENT -> RemoteBoxAlignmentsDemo()
         ScreenKeys.REMOTE_STATE_LAYOUT -> RemoteStateLayoutSimpleDemo()
+        ScreenKeys.PADDING -> PaddingDemo()
         ScreenKeys.CLICKABLE -> ClickableDemo()
         ScreenKeys.COMBINED_CLICKABLE -> CombinedClickableDemo()
         ScreenKeys.TOUCH_ACTION -> TouchActionDemo()
@@ -76,6 +79,7 @@ val Screens =
                     title = "Modifier",
                     screens =
                         listOf(
+                            ComposableScreen(key = ScreenKeys.PADDING, title = "Padding"),
                             ComposableScreen(key = ScreenKeys.CLICKABLE, title = "Clickable"),
                             ComposableScreen(
                                 key = ScreenKeys.COMBINED_CLICKABLE,
