8f97c984cad7d0371b24e7e9d9c87977d18554f5
Cary Haynie <chaynie@google.com>
2026-04-14 14:47:43 +0000

    [jxr] Config / XrCapabilities visibility cleanup
    Bug: 497017190
    Bug: 497009865
    Change-Id: I4731c4e298e1465a143630249003a9459f4a489c

--- xr/arcore/arcore-play-services/src/main/kotlin/androidx/xr/arcore/playservices/ArCoreRuntime.kt
+++ xr/arcore/arcore-play-services/src/main/kotlin/androidx/xr/arcore/playservices/ArCoreRuntime.kt
@@ -114,6 +114,8 @@ internal constructor(
         return timeSource.markNow()
     }
 
+    @OptIn(androidx.xr.runtime.PreviewSpatialApi::class)
+    @SuppressWarnings("RestrictedApiAndroidX")
     override fun configure(config: Config) {
         val arConfig = _session.config
 
@@ -280,6 +282,7 @@ internal constructor(
     internal companion object {
         const private val ARCORE_PACKAGE_NAME = "com.google.ar.core"
 
+        @SuppressWarnings("RestrictedApiAndroidX")
         internal val SUPPORTED_CONFIG_MODES: Set<ConfigMode> =
             setOf(
                 CameraFacingDirection.WORLD,
