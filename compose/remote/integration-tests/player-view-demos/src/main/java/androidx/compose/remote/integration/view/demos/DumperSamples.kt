9691fb627e567681a06f6cc3721db5572c60ada6
Gurchetan Singh <gurchetansingh@google.com>
2026-05-05 16:10:51 -0700

    [RemoteCompose] Introduce AiAgent sample
    The main value is of this is testing GSL shaders in
    RemoteCompose.
    
    Bug: 509574017
    Test: CI/CD
    
    Change-Id: Ie3fc519f6bdb013a735606171e552a370f68b6fd

--- compose/remote/integration-tests/player-view-demos/src/main/java/androidx/compose/remote/integration/view/demos/DumperSamples.kt
+++ compose/remote/integration-tests/player-view-demos/src/main/java/androidx/compose/remote/integration/view/demos/DumperSamples.kt
@@ -38,6 +38,7 @@ val AllSamples =
         DumperSample.Context("PressureGauge") { RemoteComposeContext(demoPressureGauge()) },
         DumperSample.Context("Countdown") { countDown() },
         DumperSample.Context("ParticleShader") { shaderFireworks() },
+        DumperSample.ComposableSample("AiAgent", @Composable @RemoteComposable { AiAgent() }),
         DumperSample.ComposableSample(
             "DogeCalendar",
             @Composable @RemoteComposable { DogeCalendar() },
