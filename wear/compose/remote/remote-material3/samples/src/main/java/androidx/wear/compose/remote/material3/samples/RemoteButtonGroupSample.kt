f87ee79d42652a214a08a67570755f880db4cddd
Yuri Schimke <yschimke@google.com>
2026-05-14 11:46:49 +0000

    Refactor Action to use RemoteAction base class
    Redesigned Action interface to remove toRemoteAction() and introduced a restricted RemoteAction abstract class. Updated HostAction, CombinedAction, ValueChange actions, PendingIntentAction, and WidgetLambdaAction to extend RemoteAction. Updated modifiers to check for RemoteAction before calling toRemoteAction().
    
    Test: ./gradlew :compose:remote:remote-creation-compose:connectedAndroidTest
    
    Relnote: N/A
    Bug: 513228889
    Change-Id: I0b15d769b5a56029f6ec9f6a13b2b30bf2d7b1f0

--- wear/compose/remote/remote-material3/samples/src/main/java/androidx/wear/compose/remote/material3/samples/RemoteButtonGroupSample.kt
+++ wear/compose/remote/remote-material3/samples/src/main/java/androidx/wear/compose/remote/material3/samples/RemoteButtonGroupSample.kt
@@ -21,7 +21,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.MailOutline
-import androidx.compose.remote.creation.compose.action.HostAction
+import androidx.compose.remote.creation.compose.action.hostAction
 import androidx.compose.remote.creation.compose.layout.RemoteComposable
 import androidx.compose.remote.creation.compose.modifier.RemoteModifier
 import androidx.compose.remote.creation.compose.modifier.fillMaxWidth
@@ -80,4 +80,4 @@ private val tonalColors
                 disabledContentColor = RemoteMaterialTheme.colorScheme.primary.copy(0.38f.rf),
             )
 
-private val testAction = HostAction("testAction".rs, 1.rf)
+private val testAction = hostAction("testAction".rs, 1.rf)
