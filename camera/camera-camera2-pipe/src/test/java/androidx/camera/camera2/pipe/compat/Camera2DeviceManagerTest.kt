ce631d717dff9a1d24ffb7fb08c9d01db0051dfb
Jasmine Chen <lnishan@google.com>
2026-05-28 16:41:02 -0700

    Rename PruningCamera2DeviceManager to *Impl
    Renamed PruningCamera2DeviceManager to Camera2DeviceManagerImpl in
    order to follow the standard Impl naming convention. Also updated all
    occurrences in tests, configuration files, and Dagger bindings.
    
    Bug: None
    Test: ./gradlew :camera:camera-camera2-pipe:test
    Change-Id: I2c66179cb79a71a7b1831c556e6e4e397b99bbf8

--- camera/camera-camera2-pipe/src/test/java/androidx/camera/camera2/pipe/compat/Camera2DeviceManagerTest.kt
+++ camera/camera-camera2-pipe/src/test/java/androidx/camera/camera2/pipe/compat/Camera2DeviceManagerTest.kt
@@ -58,7 +58,7 @@ import org.robolectric.annotation.Config
 @RunWith(RobolectricCameraPipeTestRunner::class)
 @Config(minSdk = Build.VERSION_CODES.M)
 @OptIn(ExperimentalCoroutinesApi::class)
-internal class PruningCamera2DeviceManagerImplTest {
+internal class Camera2DeviceManagerImplTest {
     private val testScope = TestScope()
 
     private val fakeContext: Context = mock()
@@ -126,7 +126,7 @@ internal class PruningCamera2DeviceManagerImplTest {
         Camera2SystemState(CameraPipe.CameraInteropConfig(), fakeThreads)
 
     private val deviceManager =
-        PruningCamera2DeviceManager(
+        Camera2DeviceManagerImpl(
             fakePermissions,
             fakeRetryingCameraStateOpener,
             fakeCamera2DeviceCloser,
@@ -164,7 +164,7 @@ internal class PruningCamera2DeviceManagerImplTest {
         }
 
     val awaitAbortedDeviceManager =
-        PruningCamera2DeviceManager(
+        Camera2DeviceManagerImpl(
             fakePermissions,
             awaitAbortedRetryingCameraStateOpener,
             fakeCamera2DeviceCloser,
