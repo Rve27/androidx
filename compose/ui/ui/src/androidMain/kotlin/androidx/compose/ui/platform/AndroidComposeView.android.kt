c032523a7404cbc03a83bad6d5e05c0496298934
Andrei Shikov <ashikov@google.com>
2026-06-08 18:18:07 +0100

    Remove outOfFrame callbacks in benchmark environment.
    The callbacks are never flushed and retain a reference to the view, which results in OOM on some runners. We don't want to completely unregister some of those actions on view detached signal since it might be used for cleanup / disposal.
    
    Test: Test run on oriole target
    Fixes: 521240337
    Change-Id: Ic3ffce664aebaf5ac1db737f61fed28908d6f0f1

--- compose/ui/ui/src/androidMain/kotlin/androidx/compose/ui/platform/AndroidComposeView.android.kt
+++ compose/ui/ui/src/androidMain/kotlin/androidx/compose/ui/platform/AndroidComposeView.android.kt
@@ -1962,6 +1962,10 @@ internal class AndroidComposeView(context: Context, composeViewContext: ComposeV
         composeAccessibilityDelegate.processSemanticChangesForTest()
     }
 
+    override fun clearCallbacks() {
+        handler.removeCallbacks(outOfFrameRunnable)
+    }
+
     override fun setUncaughtExceptionHandler(handler: RootForTest.UncaughtExceptionHandler?) {
         uncaughtExceptionHandler = handler
         measureAndLayoutDelegate.uncaughtExceptionHandler = handler
