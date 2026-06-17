a748f688bbc2dcfd08557f8a6539b27658add834
Samrat Debroy <samratdebroy@google.com>
2026-06-17 15:21:57 -0700

    Revert "Implement `toString` overrides for scenecore enum-like classes"
    This reverts commit 15c334804b5ba959799e4102c905e1bad93e6975.
    
    Reason for revert: Presubmits are breaking due to missing experimental annotations https://android-build.corp.google.com/build_explorer/artifact_viewer/P127477783/androidx_incremental/logs/build_error.log?attemptId=latest
    
    BUG: 520081710
    Change-Id: Id3520446e468580cb0b028a259b59fea670b7a22

--- xr/scenecore/scenecore/src/main/java/androidx/xr/scenecore/GltfAnimation.kt
+++ xr/scenecore/scenecore/src/main/java/androidx/xr/scenecore/GltfAnimation.kt
@@ -73,14 +73,6 @@ internal constructor(
              */
             @JvmField public val PAUSED: AnimationState = AnimationState(3)
         }
-
-        override fun toString(): String =
-            when (this) {
-                PLAYING -> "PLAYING"
-                STOPPED -> "STOPPED"
-                PAUSED -> "PAUSED"
-                else -> "UNKNOWN ($value)"
-            }
     }
 
     /**
